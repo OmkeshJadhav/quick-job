527edeff67d9a9fb7f394c5b3ae79150bf03e477
Omkesh <omkeshjadhav.dev@gmail.com>
2025-11-15 22:28:13 +0530

    Authentication with supabase & Clerk

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
 import { ModeToggle } from './theme-toggle'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 const Header = () => {
     return (
@@ -13,6 +14,14 @@ const Header = () => {
             <div className='flex justify-center items-center gap-3'>
                 <ModeToggle />
                 <Button variant="outline">Login</Button>
+                {/* <div>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div> */}
             </div>
         </div>
     )
