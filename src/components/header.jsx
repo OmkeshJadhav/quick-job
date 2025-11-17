14e7de9b21b70036861630971cfe6f619269fefc
Omkesh <omkeshjadhav.dev@gmail.com>
2025-11-17 09:28:44 +0530

    Implemented user authentication using clerk

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,80 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { Button } from './ui/button'
 import ThemeToggle from './theme-toggle'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignIn, UserButton } from '@clerk/clerk-react'
+import { BriefcaseBusiness, Heart, PenBox } from 'lucide-react'
 
 const Header = () => {
-    return (
-        <div className='py-2 flex justify-between items-center'>
-            <Link to="/">
-                <img src="/job-portal-logo.png" alt="logo" className='w-44' />
-            </Link>
+    const [search, setSearch] = useSearchParams();
+    const isSignInOpen = search.get("sign-in") === "true";
 
-            <div className='flex justify-center items-center gap-3'>
-                <ThemeToggle />
-                <Button variant="outline">Login</Button>
-                {/* <div>
+    const openSignIn = () => setSearch({ "sign-in": true });
+    const closeSignIn = () => setSearch({});
+
+    const handleOverlayClick = (e) => {
+        if (e.target == e.currentTarget) {
+            closeSignIn();
+            // setSearch({})
+        }
+    }
+
+    return (
+        <>
+            <nav className='py-2 flex justify-between items-center'>
+                <Link to="/">
+                    <img src="/job-portal-logo.png" alt="logo" className='w-44' />
+                </Link>
+
+                <div className='flex gap-8'>
+                    <ThemeToggle />
                     <SignedOut>
-                        <SignInButton />
+                        <Button variant="outline" onClick={openSignIn}>Login</Button>
                     </SignedOut>
                     <SignedIn>
-                        <UserButton />
+                        <Link to="/post-job">
+                            <Button variant="destructive" className="rounded-full" >
+                                <PenBox size={20} classname="mr-2" />
+                                Post a Job
+                            </Button>
+                        </Link>
+                        <UserButton 
+                            appearance={{
+                                elements: {
+                                    avatarBox: "w-10 h-10"
+                                }
+                            }}
+                        >
+                            <UserButton.MenuItems>
+                                <UserButton.Link 
+                                    label='My Jobs'
+                                    labelIcon={<BriefcaseBusiness size={15}/>}
+                                    href='/my-jobs'
+                                />
+                                <UserButton.Link 
+                                    label='Saved Jobs'
+                                    labelIcon={<Heart size={15}/>}
+                                    href='/saved-jobs'
+                                />
+                            </UserButton.MenuItems>
+
+                        </UserButton>
+
                     </SignedIn>
-                </div> */}
-            </div>
-        </div>
+                </div>
+            </nav>
+
+            {isSignInOpen && (
+                <div
+                    className='fixed inset-0 flex items-center justify-center bg-black/80'
+                    onClick={handleOverlayClick}
+                >
+                    <SignIn
+                        signUpForceRedirectUrl='/onboarding'
+                        fallbackRedirectUrl='/onboarding'
+                    />
+                </div>
+            )}
+        </>
     )
 }
 
