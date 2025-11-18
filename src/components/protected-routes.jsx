04c8a79fd18f06b437023270053494793fde0698
Omkesh <omkeshjadhav.dev@gmail.com>
2025-11-18 09:00:59 +0530

    Conditional access/re-direction of pages based on role

--- src/components/protected-routes.jsx
+++ src/components/protected-routes.jsx
@@ -2,12 +2,22 @@ import { useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 
-const ProtectedRoutes = ({children}) => {
-    const {isSignedIn, user, isLoaded} = useUser()
+const ProtectedRoutes = ({ children }) => {
+    const { isSignedIn, user, isLoaded } = useUser()
     const { pathname } = useLocation()
 
-    if(isLoaded && !isSignedIn && isSignedIn !== undefined){
-        return <Navigate to="/?sign-in=true"/>
+    // If user is not signed in then navigate to sign-in=true so that sign in modal will open
+    if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+        return <Navigate to="/?sign-in=true" />
+    }
+
+    // Always redirect to "onboarding" page to assign role if user has no role assigned
+    if (
+        user !== undefined &&
+        !user?.unsafeMetadata?.role &&
+        pathname !== "/onboarding"
+    ) {
+        return <Navigate to="/onboarding" />
     }
 
     return children
