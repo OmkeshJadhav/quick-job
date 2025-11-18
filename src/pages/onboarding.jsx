d2d8dd2acb4942f2d4450ce4cfc26b14b9fc4e44
Omkesh <omkeshjadhav.dev@gmail.com>
2025-11-18 09:00:13 +0530

    onboarding page completed with role based navigation

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { useUser } from '@clerk/clerk-react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { BarLoader } from 'react-spinners'
 
 const OnBoarding = () => {
+    const { user, isLoaded } = useUser()
+    const navigate = useNavigate()
+
+    // Add user role to unsafeMetadata from clerk and based on that role navigate
+    const handleRoleSelection = async (role) => {
+        await user.update({
+            unsafeMetadata: { role },
+        }).then(() => {
+            navigate(role === "recruiter" ? "/post-job" : "/jobs")
+        }).catch((err) => {
+            console.log("Error uploading role", err);
+        })
+    }
+
+    // If user has a role assigned then always redirect he user to specific page
+    useEffect(() => {
+        if(user?.unsafeMetadata?.role){
+            navigate(user?.unsafeMetadata?.role === "recruiter" ? "/post-job" : "/jobs")
+        }
+    }, [user])
+
+    // Loader from react-spinners library
+    if (!isLoaded) {
+        return < BarLoader className='mb-4' width={"100%"} color='#36d7b7' />
+    }
+
+
     return (
-        <div>OnBoarding</div>
+        <div className='flex flex-col items-center justify-center mt-32'>
+            <h2 className='gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter'>
+                I am a....
+            </h2>
+            <div className='mt-16 grid grid-cols-2 gap-4 w-full md:px-40'>
+                <Button variant="blue" className="h-32 text-2xl" onClick={() => handleRoleSelection("candidate")}>
+                    Candidate
+                </Button>
+                <Button variant="destructive" className="h-32 text-2xl" onClick={() => handleRoleSelection("recruiter")}>
+                    Recruiter
+                </Button>
+            </div>
+        </div>
     )
 }
 
